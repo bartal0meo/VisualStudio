7d9c5d814a0ac644f165cc52b2e559f12436fbab
Karol <38130928+kargabry@users.noreply.github.com>
2018-04-06 14:33:59 +0200

    checkboxy

--- botstrony/botstrony/Form1.cs
+++ botstrony/botstrony/Form1.cs
@@ -15,7 +15,7 @@ namespace botstrony
     {
         private void Form1_Load(object sender, EventArgs e)
         {
-           webBrowser1.Navigate("http://mylomza.pl/ogloszenia/dodaj2.html");
+
         }
         WebBrowser webBrowser = new WebBrowser();
         public Form1()
@@ -31,7 +31,7 @@ namespace botstrony
             {
                 mylomza();
             }
-            else if(fourlomza_checkBox.Checked)
+            else if (fourlomza_checkBox.Checked)
             {
                 fourlomza();
             }
@@ -43,15 +43,15 @@ namespace botstrony
         private void localhost()
         {
             webBrowser.Navigate("http://localhost/bot/rejestracja.php");
-           // var f_name = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
-            webBrowser.Document.GetElementsByTagName("text")["nick"].SetAttribute( "input", name_textbox.Text);
+            // var f_name = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
+            webBrowser.Document.GetElementsByTagName("text")["nick"].SetAttribute("input", name_textbox.Text);
             //var f_name1 = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
             webBrowser.Document.GetElementsByTagName("text")["email"].SetAttribute("input", name_textbox.Text);
             //var f_name2 = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
             webBrowser.Document.GetElementsByTagName("text")["tel"].SetAttribute("input", name_textbox.Text);
             //var f_name3 = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
             webBrowser.Document.GetElementsByTagName("text")["kat"].SetAttribute("input", name_textbox.Text);
-          //  var f_name4 = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
+            //  var f_name4 = webBrowser.Document.GetElementsByTagName("text")["nick"].GetAttribute("value");
             webBrowser.Document.GetElementsByTagName("text")["tresc"].SetAttribute("input", name_textbox.Text);
 
             webBrowser.Document.GetElementById("submit").InvokeMember("click");
@@ -64,12 +64,12 @@ namespace botstrony
 
             //  webBrowser1.Document.GetElementById("form_nazwa").InnerText = name_textbox.Text;
 
-           // webBrowser.Document.GetElementById("contact").SetAttribute("value", phone_textbox.Text);
+            // webBrowser.Document.GetElementById("contact").SetAttribute("value", phone_textbox.Text);
 
 
-          //  webBrowser1.Document.GetElementById("cat_id").SetAttribute("value", "6");
+            //  webBrowser1.Document.GetElementById("cat_id").SetAttribute("value", "6");
 
-          //  webBrowser1.Document.GetElementById("description").InnerText = tresc_textbox.Text;
+            //  webBrowser1.Document.GetElementById("description").InnerText = tresc_textbox.Text;
 
 
             // HtmlElementCollection elc = this.webBrowser1.Document.GetElementsByTagName("input");
@@ -81,34 +81,30 @@ namespace botstrony
             //    }
             // }
 
-            
+
         }
-        
+
         private void fourlomza()
         {
-             webBrowser1.Document.GetElementById("form_email").InnerText = email_textbox.Text;
-
-        webBrowser1.Document.GetElementById("form_nazwa").InnerText = name_textbox.Text;
-
-        webBrowser1.Document.GetElementById("form_tel").InnerText = phone_textbox.Text;
-
-
-       webBrowser1.Document.GetElementById("form_kat").SetAttribute("value", "4");
-
-        webBrowser1.Document.GetElementById("form_tresc").InnerText = tresc_textbox.Text;
+            webBrowser1.Navigate("https://www.4lomza.pl/ogl2.php?mod=new");
+            webBrowser1.Document.GetElementById("form_email").InnerText = email_textbox.Text;
+            webBrowser1.Document.GetElementById("form_nazwa").InnerText = name_textbox.Text;
+            webBrowser1.Document.GetElementById("form_tel").InnerText = phone_textbox.Text;
+            webBrowser1.Document.GetElementById("form_kat").SetAttribute("value", "4");
+            webBrowser1.Document.GetElementById("form_tresc").InnerText = tresc_textbox.Text;
 
 
-        HtmlElementCollection elc = this.webBrowser1.Document.GetElementsByTagName("input");
-        foreach (HtmlElement el in elc)
-        {
-            if (el.GetAttribute("type").Equals("submit"))
+            HtmlElementCollection elc = this.webBrowser1.Document.GetElementsByTagName("input");
+            foreach (HtmlElement el in elc)
             {
-                el.InvokeMember("click");
+                if (el.GetAttribute("type").Equals("submit"))
+                {
+                    el.InvokeMember("click");
+                }
             }
+
         }
-              
-        }
-        
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             int length = tresc_textbox.TextLength;
@@ -128,6 +124,28 @@ namespace botstrony
             }
         }
 
-      
+        private void mylomza_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mylomza_checkBox.Checked)
+            {
+                webBrowser1.Navigate("http://mylomza.pl/ogloszenia/dodaj2.html");
+            }
+            else
+            {
+                webBrowser1.Navigate("");
+            }
+        }
+
+        private void fourlomza_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (fourlomza_checkBox.Checked)
+            {
+                webBrowser1.Navigate("https://www.4lomza.pl/ogl2.php?mod=new");
+            }
+            else
+            {
+                webBrowser1.Navigate("");
+            }
+        }
     }
 }
